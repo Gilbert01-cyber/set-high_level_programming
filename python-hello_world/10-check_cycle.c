e1af05718172925bc31e8b9270e13985811b4982
Gilbert01-cyber <nggilbert24@gmail.com>
2026-07-12 03:08:25 -0700

    Add 10-check_cycle.c: Floyd's cycle detection

---
+++ python-hello_world/10-check_cycle.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+int check_cycle(listint_t *list)
+{
+listint_t *slow;
+listint_t *fast;
+
+if (list == NULL)
+return (0);
+
+slow = list;
+fast = list;
+
+while (fast != NULL && fast->next != NULL)
+{
+slow = slow->next;
+fast = fast->next->next;
+
+if (slow == fast)
+return (1);
+}
+
+return (0);
+}
