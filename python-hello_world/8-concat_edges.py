d84a3acde81a0b7e0261f811e617055274f149ca
Gilbert01-cyber <nggilbert24@gmail.com>
2026-07-12 02:49:39 -0700

    Fix 8-concat_edges.py with correct slicing

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-str = "object-oriented programming with Python is a great combination\
- of clarity and power for building things"
-str = str[:39]
+str = "Python is an interpreted, interactive, object-oriented programming\
+language that combines remarkable power with very clear syntax"
+str = str[39:66] + str[105:111] + str[0:6]
 print(str)
