a28757866c88bb34e4d65c375f59adc5e4d72cd5
Gilbert NDAGIJIMANA <nggilbert24@gmail.com>
2026-07-27 18:22:20 +0200

    Add docstring to LockedClass for clarity
    Added docstring to explain class functionality.

--- python-everything_is_object/101-locked_class.py
+++ python-everything_is_object/101-locked_class.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
+"""Module that defines a class with restricted attribute creation."""
+
+
 class LockedClass:
+    """Represent a class that prevents dynamic attribute creation.
+
+    The only attribute allowed to be set is `first_name`.
+    """
+
     __slots__ = ['first_name']
