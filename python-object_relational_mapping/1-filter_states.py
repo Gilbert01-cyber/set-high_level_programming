2bfc8d7d529bdc0e7434af6b4ff7a28ce1af57a8
Gilbert NDAGIJIMANA <nggilbert24@gmail.com>
2026-08-19 21:27:16 +0000

    Fix line length for pycodestyle

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -17,7 +17,9 @@ if __name__ == "__main__":
         db=db_name
     )
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC")
+    cursor.execute(
+        "SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC"
+    )
     rows = cursor.fetchall()
     for row in rows:
         print(row)
