2c7a811704ac0f305cd7c5799d828e6cbbe9aebe
Gilbert NDAGIJIMANA <nggilbert24@gmail.com>
2026-08-20 10:53:56 +0200

    Add append_write function for appending text to file
    Defines a function to append text to a UTF8 file.

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module that defines a function to append text to a UTF8 file."""
+
+
+def append_write(filename="", text=""):
+    """Append a string to a text file (UTF8) and return chars added."""
+    with open(filename, mode="a", encoding="utf-8") as f:
+        return f.write(text)
