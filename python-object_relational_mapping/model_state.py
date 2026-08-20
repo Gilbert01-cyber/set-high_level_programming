254c66ff4b56c48e1977b4301a8ccf9d4cb7536c
Gilbert NDAGIJIMANA <nggilbert24@gmail.com>
2026-08-20 07:20:30 +0200

    Add State class for MySQL states table

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Defines the State class mapped to the MySQL states table."""
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+Base = declarative_base()
+
+
+class State(Base):
+    """Represents a state in the MySQL database."""
+    __tablename__ = "states"
+    id = Column(Integer, primary_key=True, unique=True, nullable=False,
+                autoincrement=True)
+    name = Column(String(128), nullable=False)
