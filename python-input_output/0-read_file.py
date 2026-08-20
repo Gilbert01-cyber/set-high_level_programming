f0684ba921e50ccd49aaba5cf5d6e25d91fb7a77
Gilbert NDAGIJIMANA <nggilbert24@gmail.com>
2026-08-20 10:45:05 +0200

    Add read_file function to read UTF8 text files
    Defines a function to read and print a UTF8 text file.

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module that defines a function to read a UTF8 text file and print it."""
+
+
+def read_file(filename=""):
+    """Read a text file (UTF8) and print its content to stdout."""
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
